f105b81bfe534b2c4d9dd6ce3275f2d0aa229c54
snehakbgit <snehapandey8340@gmail.com>
2025-12-29 22:23:27 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,4 @@
+print("hey there! welcome to the band name generator")
+city=input("what is the name of your city?\n")
+pet=input("what is the name of your pet?\n")
+print("your band name could be " + city + " " + pet)
