d4d0b73ec3dd258c9e7aa4669cd737a254b1c005
snehakbgit <snehapandey8340@gmail.com>
2025-12-30 00:01:13 +0530

    add day 02

---
+++ Day 02/dataTypes.py
@@ -0,0 +1,16 @@
+# string
+print("123dvsjv")
+
+# int
+print(123)
+
+# large integer
+print(123_343_877)
+
+# float
+print(2.567)
+
+# boolean
+print(True)
+print(False)
+
---
+++ Day 02/dataTypesFunc.py
@@ -0,0 +1,12 @@
+# len(123) - len funtion is only for string or equivalent
+
+# type
+print(type(123))
+
+print(type("ASA123"))
+
+print(type(123_123_888))
+
+print(type(1.2324))
+
+print(type(True))
