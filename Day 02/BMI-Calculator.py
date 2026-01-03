a0502300d2aa59459ae642e46aab01054ff001a6
snehakbgit <snehapandey8340@gmail.com>
2026-01-03 15:41:56 +0530

    Add day 2 project

---
+++ Day 02/BMI-Calculator.py
@@ -0,0 +1,9 @@
+# name = input("Enter Your Name:")
+# length_name = len(name)
+
+# print("Numbers of the letter in your name: " + str(length_name))
+
+h = 1.84
+w = 80
+bmi = w/h**2
+print(bmi)
--- Day 02/task-1.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter Your Name:")
-length_name = len(name)
-
-print("Numbers of the letter in your name: " + str(length_name))
